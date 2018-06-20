cac7bc05db3d8ee5e300617ed86131b2d3c81b85
Ivan <elreizerwtf@gmail.com>
2018-06-20 18:21:21 -0300

    Añadiendo el archivo Test.py

---
+++ Test.py
@@ -0,0 +1,42 @@
+respuesta_ganadora = 25
+
+respuesta_del_usuario = int(input("Elige un numero del 1 al 50:"))
+
+if respuesta_ganadora == respuesta_del_usuario:
+    print("ganaste")
+
+else:
+    print("No, otra vez")
+
+respuesta_del_usuario = int(input("Elige un numero del 1 al 50:"))
+
+if respuesta_ganadora == respuesta_del_usuario:
+    print("ganaste")
+
+else:
+    print("No, otra vez")
+
+respuesta_del_usuario = int(input("Elige un numero del 1 al 50:"))
+
+if respuesta_ganadora == respuesta_del_usuario:
+    print("ganaste")
+
+else:
+    print("No, otra vez")
+
+respuesta_del_usuario = int(input("Elige un numero del 1 al 50:"))
+
+if respuesta_ganadora == respuesta_del_usuario:
+    print("ganaste")
+
+else:
+    print("No, otra vez")
+
+respuesta_del_usuario = int(input("Elige un numero del 1 al 50:"))
+
+if respuesta_ganadora == respuesta_del_usuario:
+    print("ganaste")
+
+else:
+    print("perdiste, era 25 el numero")
+
